c734f5473adf708123eb6bf7f408d5e0d557d976
Golshan Piroozzadeh <47850735+golshan1993@users.noreply.github.com>
2021-01-21 11:11:18 +0330

    Using RXbinding for recognizing double click.

---
+++ app/src/main/java/com/example/clickstream/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.clickstream;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.jakewharton.rxbinding4.view.RxView;
+
+import java.util.Observable;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+
+public class MainActivity extends Activity {
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button)findViewById(R.id.button);
+        final int numClicks = 2;
+        final int timeout = 1000;
+        RxView.clicks(btn)
+       .buffer(1000, TimeUnit.MILLISECONDS)
+                .filter(clicks -> clicks.size() == numClicks)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(clicks -> {
+                    Log.d("CLICK", "CLICK");
+                });
+    }
+}
